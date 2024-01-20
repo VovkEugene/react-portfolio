0c9ffb74b490d9db1514a32982b9d8d60f628eb0
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-01-20 15:33:51 +0300

    create Contact component

---
+++ src/componets/Contact/index.js
@@ -0,0 +1,67 @@
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { useEffect, useState } from 'react'
+
+function Contact() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              stringArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              index={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+          </p>
+          <form className='contact-form'>
+            <ul>
+              <li className="half">
+                <input type="text" name="name" placeholder="Name" required />
+              </li>
+              <li className="half">
+                <input type="email" name="email" placeholder="Email" required />
+              </li>
+              <li>
+                <input
+                  type="text"
+                  name="subject"
+                  placeholder="Subject"
+                  required
+                />
+              </li>
+              <li>
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  required
+                ></textarea>
+              </li>
+              <li>
+                <input type="submit" value="Send" className="flat-button" />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
