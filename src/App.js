88f27d49728acc4556c61c3307ebaa9b1444abd2
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-01-20 11:19:01 +0300

    create About component and pacman Loader

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './componets/Layout'
 import Home from './componets/Home'
+import About from './componets/About'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
       </Routes>
     </>
   )
--- src/componets/Home/index.js
+++ src/componets/Home/index.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/logo-s.png'
-import './index.scss'
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
 import Logo from './Logo'
+import './index.scss'
 
 function Home() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -26,40 +27,45 @@ function Home() {
   ]
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 4000)
+
+    return clearTimeout(timer)
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            stringArray={nameArray}
-            index={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            stringArray={jobArray}
-            index={22}
-          />
-        </h1>
-        <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
-        <Link to="/contact" className="flat-button">
-          Contact me
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              stringArray={nameArray}
+              index={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              stringArray={jobArray}
+              index={22}
+            />
+          </h1>
+          <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
+          <Link to="/contact" className="flat-button">
+            Contact me
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 export default Home
